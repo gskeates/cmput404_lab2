a5f12d85709a61b0a6834b166c7721b2f53144a9
gskeates <gskeates@ualberta.ca>
2021-01-21 20:03:31 -0700

    echo server forks

--- echo_server.py
+++ echo_server.py
@@ -1,32 +1,41 @@
 #!/usr/bin/env python3
 import socket
 import time
+from multiprocessing import Process
 
-#define address & buffer size
+
+# define address & buffer size
 HOST = ""
 PORT = 8001
 BUFFER_SIZE = 1024
 
+def client_connection(conn, addr):
+    print("Connected by", addr)
+    # recieve data, wait a bit, then send it back
+    full_data = conn.recv(BUFFER_SIZE)
+    print(full_data)
+    time.sleep(0.5)
+    conn.sendall(full_data)
+
+
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    
-        #QUESTION 3
+
+        # QUESTION 3
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        
-        #bind socket to address
+
+        # bind socket to address
         s.bind((HOST, PORT))
-        #set to listening mode
+        # set to listening mode
         s.listen(2)
-        
-        #continuously listen for connections
+
+        # continuously listen for connections
         while True:
             conn, addr = s.accept()
-            print("Connected by", addr)
-            
-            #recieve data, wait a bit, then send it back
-            full_data = conn.recv(BUFFER_SIZE)
-            time.sleep(0.5)
-            conn.sendall(full_data)
+
+            # Spawn new process to deal with connection
+            process = Process(target=client_connection, args=(conn, addr))
+            process.start()
             conn.close()
 
 if __name__ == "__main__":
